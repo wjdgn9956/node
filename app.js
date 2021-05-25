77995599e6719b8f8331323c8beddfd6dc6e3328
wjdgn9956 <wjdgn9956@naver.com>
2021-05-25 11:08:11 +0900

    app.js 기본 세팅

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+dotenv.config();
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+    express:app,
+    watch:true,
+})
+
+
+app.use(morgan("dev"));
+
+
+
+// 없는 페이지 처리 미들웨어 (라우터)
+app.use((res, req, next) => {
+    const error = new Error(`${req.method} ${req.url}는 없는 페이지 입니다.`);
+    error.status = 404;
+    next(error); // 에러 처리 미들웨어
+})
+
+
+
+// 에러 처리 미들웨어(라우터)
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status || 500).render("error");
+})
+
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "번 포트에서 서버 대기중");
+})
