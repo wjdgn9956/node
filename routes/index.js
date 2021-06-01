2b8d78df3ac49298f08fed76b81c561b1fc665ce
wjdgn9956 <wjdgn9956@naver.com>
2021-06-01 12:59:28 +0900

    asdsa

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const resume = require("../models/resume");
 router.get("/", async (req, res, next) => {
 
     const data = await resume.get();
-    console.log(data);
     res.render("main", data);
 });
 
